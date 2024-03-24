f911193355c46d22639980f0f763292b3e42ba91
SimpleCopper <89261212+SimpleCopper@users.noreply.github.com>
2024-03-25 06:59:42 +0700

    home bg

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from '../Nav/Navbar';
+import './css/Home.css'
+import BG from '../Asset/BG.jpg';
 
 function Home() {
   return (
-    <div>
+    <div className="home-container">
       <Navbar/>
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <h1 className="home-text">
+          Start trading with us
+        </h1>
+      </div>
     </div>
   )
 }
